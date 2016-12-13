4400650ee1d51ed923422f5627d3f020834842e0
Lutsko Michael <lutsko86@gmail.com>
2016-12-13 18:14:38 +0200

    first

--- src/components/Image.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Image extends Component {
-    render(){
-        return (
-            <img src={require(`../assets/images/${this.props.img}`)} style={{width: '100%', height: '305px'}} alt='Post'/>
-        )
-    }
-}
