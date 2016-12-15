efbbd5410f22001d3889a2bb2a362cd81a54750f
Lutsko Michael <lutsko86@gmail.com>
2016-12-15 08:38:54 +0200

    Lesson 01

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 
+const styles = {
+    width: '100%',
+    height: '305px'
+};
+
+const desc = 'Post';
+
 export default class Image extends Component {
     render(){
         return (
-            <img src={require(`../assets/images/${this.props.img}`)} style={{width: '100%', height: '305px'}} alt='Post'/>
+            <img src={require(`../assets/images/${this.props.img}`)} style={styles} alt={desc}/>
         )
     }
 }
