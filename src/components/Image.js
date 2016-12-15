ea4ac1e680b677296b38fed50990f404da6aba05
Lutsko Michael <lutsko86@gmail.com>
2016-12-15 10:19:41 +0200

    fix images styles

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
 
-const styles = {
-    width: '100%',
-    height: '305px'
-};
-
-const desc = 'Post';
 
 export default class Image extends Component {
     render(){
         return (
-            <img src={require(`../assets/images/${this.props.img}`)} style={styles} alt={desc}/>
+            <img {...this.props.img} />
         )
     }
 }
--- src/constants/BlogItem.js
+++ src/constants/BlogItem.js
@@ -1,13 +1,29 @@
 export const items = [
     {
-        img: '1.jpg',
+        img: {
+            src: require('../assets/images/1.jpg'),
+            width: '100%',
+            height: '305px',
+            alt: 'Post one'
+        },
         text: 'Sed euismod feugiat sodales. Vivamus dui ipsum, laoreet vitae euismod sit amet, euismod ac est. Sed turpis massa, convallis vitae facilisis eget, malesuada ullamcorper nibh. Nunc pulvinar augue non felis dictum ultricies. Donec lacinia, enim sit amet volutpat sodales, lorem velit fringilla metus, et semper metus sapien non odio. Nulla facilisi.Praesent gravida suscipit leo, eget fermentum magna malesuada ac. Maecenas pulvinar malesuada elementum.'
     },
     {
-        img: '2.jpg',
+        img: {
+            src: require('../assets/images/2.jpg'),
+            width: '100%',
+            height: '305px',
+            alt: 'Post two'
+        },
         text: 'Phasellus vel arcu vitae neque sagittis aliquet ac at purus. Vestibulum varius eros in dui sagittis non ultrices orci hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
-        img: '3.jpg',
-        text: 'Nunc pulvinar augue non felis dictum ultricies. Donec lacinia, enim sit amet volutpat sodales.'}
+        img: {
+            src: require('../assets/images/3.jpg'),
+            width: '100%',
+            height: '305px',
+            alt: 'Post three'
+        },
+        text: 'Nunc pulvinar augue non felis dictum ultricies. Donec lacinia, enim sit amet volutpat sodales.'
+    }
 ];
